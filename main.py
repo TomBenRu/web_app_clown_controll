6719ffc06c2860d24148ded21d296e17b7b5c0a4
tombe <mail@thomas-ruff.de>
2024-01-02 05:17:25 +0100

    In progress.

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from database import db
 from routers import websocket, department
 
 
-db.start_db()
-
 app = FastAPI()
 
 app.include_router(websocket.router)
 app.include_router(department.router)
+
+db.start_db()
 
 
 def run():
