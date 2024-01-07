e872f542fe9bde1db769606b11d5ed905f13942a
tombe <mail@thomas-ruff.de>
2024-01-07 22:56:36 +0100

    In progress.

--- test_htmx/main.py
+++ test_htmx/main.py
@@ -1,6 +1,6 @@
 import datetime
 
-from fastapi import FastAPI, Request, Header, WebSocket, WebSocketDisconnect, Query
+from fastapi import FastAPI, Request, Header, WebSocket, WebSocketDisconnect, Query, Cookie
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
@@ -18,25 +18,28 @@ def index(request: Request, hx_request: str | None = Header(default=None)):
     ]
     if hx_request:
         return templates.TemplateResponse('table.html.j2', context={'request': request, 'films': films})
-    return templates.TemplateResponse('index2.html.j2', context={'request': request, 'films': films, 'token': '56789'})
+    response = templates.TemplateResponse('index2.html.j2', context={'request': request, 'films': films})
+    response.set_cookie(key='ws-cookie', value='my-ws-token')
+    return response
 
 
 @app.websocket('/ws/notifications/')
-async def websocket_endpoint(websocket: WebSocket, token: str = Query()):
-    print(f'{token=}')
+async def websocket_endpoint(websocket: WebSocket):
+    print('------------------------in---------------------------')
     await websocket.accept()
+    print(f'{websocket.headers=}')
+    print(f'{websocket.cookies=}')
     try:
         while True:
             data = await websocket.receive_json()
             print(f'{data=}')
-            user_id = data['user-id']
             await websocket.send_text(f"""
             <form id="form" ws-send>
-                <div><input  class="hidden" name="user-id" value="1234"><input  class="bg-green-300" name="chat-message"></div>
+                <div><input  class="bg-green-300" name="chat-message"></div>
                 <div><button class="bg-blue-300 shadow-lg" ws-send>send</button></div>
             </form>
             <div id="notifications" hx-swap-oob="afterbegin">
-                <p>{datetime.datetime.now(): %d.%m.%y %H:%M:%S}: Neue Nachricht von ws to {user_id}.</p>
+                <p>{datetime.datetime.now(): %d.%m.%y %H:%M:%S}: Neue Nachricht von ws to {websocket.cookies}.</p>
             </div>""")
     except WebSocketDisconnect:
         print('Client is disconnected')
