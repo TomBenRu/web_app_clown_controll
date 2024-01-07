52e40b58d92fe11fd3095d5bddecff67ed7acf15
tombe <mail@thomas-ruff.de>
2024-01-07 04:42:45 +0100

    In progress.

--- test_htmx/main.py
+++ test_htmx/main.py
@@ -1,6 +1,6 @@
 import datetime
 
-from fastapi import FastAPI, Request, Header, WebSocket, WebSocketDisconnect
+from fastapi import FastAPI, Request, Header, WebSocket, WebSocketDisconnect, Query
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
@@ -18,22 +18,25 @@ def index(request: Request, hx_request: str | None = Header(default=None)):
     ]
     if hx_request:
         return templates.TemplateResponse('table.html.j2', context={'request': request, 'films': films})
-    return templates.TemplateResponse('index2.html.j2', context={'request': request, 'films': films})
+    return templates.TemplateResponse('index2.html.j2', context={'request': request, 'films': films, 'token': '56789'})
 
 
 @app.websocket('/ws/notifications/')
-async def websocket_endpoint(websocket: WebSocket):
+async def websocket_endpoint(websocket: WebSocket, token: str = Query()):
+    print(f'{token=}')
     await websocket.accept()
     try:
         while True:
             data = await websocket.receive_json()
-            print(f'{data["chat-message"]=}')
+            print(f'{data=}')
+            user_id = data['user-id']
             await websocket.send_text(f"""
             <form id="form" ws-send>
-                <input name="chat-message">
+                <div><input  class="hidden" name="user-id" value="1234"><input  class="bg-green-300" name="chat-message"></div>
+                <div><button class="bg-blue-300 shadow-lg" ws-send>send</button></div>
             </form>
             <div id="notifications" hx-swap-oob="afterbegin">
-                <p>{datetime.datetime.now(): %d.%m.%y %H:%M:%S}: Neue Nachricht von ws.</p>
+                <p>{datetime.datetime.now(): %d.%m.%y %H:%M:%S}: Neue Nachricht von ws to {user_id}.</p>
             </div>""")
     except WebSocketDisconnect:
         print('Client is disconnected')
