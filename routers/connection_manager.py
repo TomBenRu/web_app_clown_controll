40242f6403c28abdd55f6ef6730a84102f4d7e82
tombe <mail@thomas-ruff.de>
2024-01-20 03:57:52 +0100

    In progress.

--- routers/connection_manager.py
+++ routers/connection_manager.py
@@ -58,7 +58,8 @@ manager = ConnectionManager()
 
 class MessageHandler:
     @staticmethod
-    async def handle_message(data: str, websocket: WebSocket, token_data: schemas.TokenData):
+    async def handle_message(data: str, websocket: WebSocket, token_data: schemas.TokenData,
+                             team_of_actors: schemas.TeamOfActorsShow | None):
         now = datetime.datetime.now(tz=datetime.timezone(datetime.timedelta(hours=1), 'Europe/Berlin')).strftime('%H:%M:%S')
         user = db_services.User.get(token_data.id)
         print(f'{token_data=}')
@@ -70,15 +71,17 @@ class MessageHandler:
             await manager.send_personal_department_message(message_personal, websocket)
             await manager.send_personal_department_message(empty_input, websocket)
         else:
+            actors = ', '.join([a.artist_name for a in team_of_actors.actors])
             message_broadcast = templates.get_template('responses/clown_response.html.j2').render(time=now, message=data)
-            alert_message_rsv = templates.get_template('responses/alert_message_received.html').render(team=f'Clowns-Team {user.artist_name}')
+            alert_message_rsv = templates.get_template('responses/alert_message_received.html').render(team=f'Clowns-Team: {actors}')
             message_personal = f'You sent: {data}'
             await manager.broadcast_departments(alert_message_rsv, websocket)
             await manager.broadcast_departments(message_broadcast, websocket)
             await manager.send_personal_clowns_team_message(message_personal, websocket)
 
     @staticmethod
-    async def user_joined_message(token_data: schemas.TokenData, websocket: WebSocket):
+    async def user_joined_message(token_data: schemas.TokenData, websocket: WebSocket,
+                                  team_of_actors: schemas.TeamOfActorsShow | None):
         user = db_services.User.get(token_data.id)
         if 'department' in token_data.authorizations:
             await manager.connect(websocket, True)
@@ -86,18 +89,21 @@ class MessageHandler:
             await manager.send_alert_to_clown_teams(websocket, message)
         else:
             await manager.connect(websocket, False)
-            message = templates.get_template('responses/alert_clowns_team_joined.html.j2').render(team=f'Clowns-Team {user.artist_name}')
+            actors = ', '.join([a.artist_name for a in team_of_actors.actors])
+            message = templates.get_template('responses/alert_clowns_team_joined.html.j2').render(team=f'Clowns-Team: {actors}')
             await manager.send_alert_to_departments(websocket, message)
 
     @staticmethod
-    async def user_leave_message(token_data: schemas.TokenData, websocket):
+    async def user_leave_message(token_data: schemas.TokenData, websocket,
+                                 team_of_actors: schemas.TeamOfActorsShow | None):
         user = db_services.User.get(token_data.id)
         if 'department' in token_data.authorizations:
             message = f'{user.name} has just left.'
             await manager.send_alert_to_clown_teams(websocket, message)
             manager.disconnect(websocket, True)
         else:
-            message = templates.get_template('responses/alert_clowns_team_left.html.j2').render(team=f'Clowns-Team {user.artist_name}')
+            actors = ', '.join([a.artist_name for a in team_of_actors.actors])
+            message = templates.get_template('responses/alert_clowns_team_left.html.j2').render(team=f'Clowns-Team: {actors}')
             await manager.send_alert_to_departments(websocket, message)
             manager.disconnect(websocket, False)
 
--- routers/websocket.py
+++ routers/websocket.py
@@ -2,6 +2,7 @@ import json
 
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect, status
 
+from database import db_services, schemas
 from database.enums import AuthorizationTypes
 from oaut2_authentication import authentication
 from routers.connection_manager import manager, MessageHandler
@@ -13,25 +14,26 @@ router = APIRouter(tags=['Web-Socket'])
 async def websocket_endpoint(websocket: WebSocket):  # todo: user muss sich mit AuthorizationType anmelden
 
     token = websocket.cookies['clown-call-auth']
-
-    print(f'{token=}')
+    team_of_actors: schemas.TeamOfActorsShow | None = None
     try:
         token_data = authentication.verify_access_token(AuthorizationTypes.department, token)
     except Exception as e:
         try:
             token_data = authentication.verify_access_token(AuthorizationTypes.actor, token)
+            team_of_actors_id = websocket.headers.get("team_of_actors_id")
+            team_of_actors = db_services.Actor.get_team_of_actors(team_of_actors_id)
         except Exception as e:
             await websocket.close(code=status.WS_1008_POLICY_VIOLATION)
             return
     print(f'{token_data=}')
 
-    await MessageHandler.user_joined_message(token_data, websocket)
+    await MessageHandler.user_joined_message(token_data, websocket, team_of_actors)
     try:
         while True:
             data = await websocket.receive_text()
             print(f'{token=}')
             print(f'{json.loads(data)=}')
             message = json.loads(data)['chat-message']
-            await MessageHandler.handle_message(message, websocket, token_data)
+            await MessageHandler.handle_message(message, websocket, token_data, team_of_actors)
     except WebSocketDisconnect as e:
-        await MessageHandler.user_leave_message(token_data, websocket)
+        await MessageHandler.user_leave_message(token_data, websocket, team_of_actors)
