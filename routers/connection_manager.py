1ab4f807d98184041f1011bf7c72b87b97fb62c6
tombe <mail@thomas-ruff.de>
2024-01-21 13:50:28 +0100

    In progress.

--- routers/connection_manager.py
+++ routers/connection_manager.py
@@ -1,4 +1,5 @@
 import datetime
+from collections import defaultdict
 from uuid import UUID
 
 from fastapi import WebSocket
@@ -11,23 +12,25 @@ templates = Jinja2Templates('templates')
 
 class ConnectionManager:
     def __init__(self):
-        self.active_department_connections: list[WebSocket] = []
-        self.active_clowns_teams_connections: list[WebSocket] = []
+        self.active_department_connections: defaultdict[UUID, list] = defaultdict(list)
+        self.active_clowns_teams_connections: defaultdict[UUID, list] = defaultdict(list)
 
-    async def connect(self, websocket: WebSocket, department: bool):
+    async def connect(self, websocket: WebSocket, department: bool, location_id: UUID):
         await websocket.accept()
         if department:
-            self.active_department_connections.append(websocket)
+            self.active_department_connections[location_id].append(websocket)
+            print(f'{self.active_department_connections=}')
         else:
-            self.active_clowns_teams_connections.append(websocket)
+            self.active_clowns_teams_connections[location_id].append(websocket)
+            print(f'{self.active_clowns_teams_connections=}')
 
-    def disconnect(self, websocket: WebSocket, department: bool):
+    def disconnect(self, websocket: WebSocket, department: bool, location_id: UUID):
         if department:
-            self.active_department_connections.remove(websocket)
-            print(f'-------------------------------------------------------------{self.active_department_connections=}')
+            self.active_department_connections[location_id].remove(websocket)
+            print(f'{self.active_department_connections=}')
         else:
-            self.active_clowns_teams_connections.remove(websocket)
-            print(f'-----------------------------------------------------------{self.active_clowns_teams_connections=}')
+            self.active_clowns_teams_connections[location_id].remove(websocket)
+            print(f'{self.active_clowns_teams_connections=}')
 
     async def send_personal_department_message(self, message: str, websocket: WebSocket):
         await websocket.send_text(message)
@@ -35,22 +38,22 @@ class ConnectionManager:
     async def send_personal_clowns_team_message(self, message: str, websocket: WebSocket):
         await websocket.send_text(message)
 
-    async def broadcast_departments(self, message: str, original_websocket: WebSocket):
-        for connection in self.active_department_connections:
+    async def broadcast_departments(self, message: str, original_websocket: WebSocket, location_id: UUID):
+        for connection in self.active_department_connections[location_id]:
             await connection.send_text(message)
 
-    async def broadcast_clowns_teams(self, message: str, original_websocket: WebSocket):
-        for connection in self.active_clowns_teams_connections:
+    async def broadcast_clowns_teams(self, message: str, original_websocket: WebSocket, location_id: UUID):
+        for connection in self.active_clowns_teams_connections[location_id]:
             await connection.send_text(message)
 
-    async def send_alert_to_departments(self, websocket: WebSocket, message: str):
+    async def send_alert_to_departments(self, websocket: WebSocket, message: str, location_id: UUID):
         print(f'{websocket.headers=}')
-        for ws in self.active_department_connections:
+        for ws in self.active_department_connections[location_id]:
             print(f'{ws.headers=}')
             await ws.send_text(message)
 
-    async def send_alert_to_clown_teams(self, websocket: WebSocket, message: str):
-        for ws in self.active_clowns_teams_connections:
+    async def send_alert_to_clown_teams(self, websocket: WebSocket, message: str, location_id: UUID):
+        for ws in self.active_clowns_teams_connections[location_id]:
             await ws.send_text(message)
 
 
@@ -60,7 +63,7 @@ manager = ConnectionManager()
 class MessageHandler:
     @staticmethod
     async def handle_message(data: str, websocket: WebSocket, token_data: schemas.TokenData,
-                             team_of_actors: schemas.TeamOfActorsShow | None):
+                             team_of_actors: schemas.TeamOfActorsShow | None, location_id: UUID):
         now = datetime.datetime.now(tz=datetime.timezone(datetime.timedelta(hours=1),
                                                          'Europe/Berlin')).strftime('%H:%M:%S')
         user = db_services.User.get(token_data.id)
@@ -69,46 +72,46 @@ class MessageHandler:
             empty_input = templates.get_template('responses/empty_message_input.html').render()
             message_personal = templates.get_template('responses/clown_call_message.html.j2').render(
                 time=now, message=data)
-            await manager.broadcast_clowns_teams(message_broadcast, websocket)
+            await manager.broadcast_clowns_teams(message_broadcast, websocket, location_id)
             await manager.send_personal_department_message(message_personal, websocket)
             await manager.send_personal_department_message(empty_input, websocket)
         else:
             actors = ', '.join([a.artist_name for a in team_of_actors.actors])
             message_broadcast = templates.get_template('responses/clown_response.html.j2').render(
-                time=now, message=data)
+                time=now, message=data, clowns_team=actors)
             alert_message_rsv = templates.get_template(
                 'responses/alert_message_received.html').render(team=f'Clowns-Team: {actors}')
             message_personal = f'You sent: {data}'
-            await manager.broadcast_departments(alert_message_rsv, websocket)
-            await manager.broadcast_departments(message_broadcast, websocket)
+            await manager.broadcast_departments(alert_message_rsv, websocket, location_id)
+            await manager.broadcast_departments(message_broadcast, websocket, location_id)
             await manager.send_personal_clowns_team_message(message_personal, websocket)
 
     @staticmethod
     async def user_joined_message(token_data: schemas.TokenData, websocket: WebSocket,
-                                  team_of_actors: schemas.TeamOfActorsShow | None):
+                                  team_of_actors: schemas.TeamOfActorsShow | None, location_id: UUID):
         user = db_services.User.get(token_data.id)
         if 'department' in token_data.authorizations:
-            await manager.connect(websocket, True)
+            await manager.connect(websocket, True, location_id)
             message = f'{user.name} has just joined.'
-            await manager.send_alert_to_clown_teams(websocket, message)
+            await manager.send_alert_to_clown_teams(websocket, message, location_id)
         else:
-            await manager.connect(websocket, False)
+            await manager.connect(websocket, False, location_id)
             actors = ', '.join([a.artist_name for a in team_of_actors.actors])
             message = templates.get_template('responses/alert_clowns_team_joined.html.j2').render(team=f'Clowns-Team: {actors}')
-            await manager.send_alert_to_departments(websocket, message)
+            await manager.send_alert_to_departments(websocket, message, location_id)
 
     @staticmethod
     async def user_leave_message(token_data: schemas.TokenData, websocket,
-                                 team_of_actors: schemas.TeamOfActorsShow | None):
+                                 team_of_actors: schemas.TeamOfActorsShow | None, location_id: UUID):
         user = db_services.User.get(token_data.id)
         if 'department' in token_data.authorizations:
             message = f'{user.name} has just left.'
-            await manager.send_alert_to_clown_teams(websocket, message)
-            manager.disconnect(websocket, True)
+            await manager.send_alert_to_clown_teams(websocket, message, location_id)
+            manager.disconnect(websocket, True, location_id)
         else:
             actors = ', '.join([a.artist_name for a in team_of_actors.actors])
 
             message = templates.get_template('responses/alert_clowns_team_left.html.j2').render(team=f'Clowns-Team: {actors}')
-            await manager.send_alert_to_departments(websocket, message)
-            manager.disconnect(websocket, False)
+            await manager.send_alert_to_departments(websocket, message, location_id)
+            manager.disconnect(websocket, False, location_id)
 
--- routers/websocket.py
+++ routers/websocket.py
@@ -1,4 +1,5 @@
 import json
+from uuid import UUID
 
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect, status
 
@@ -15,24 +16,27 @@ async def websocket_endpoint(websocket: WebSocket):  # todo: user muss sich mit 
 
     token = websocket.cookies['clown-call-auth']
     team_of_actors: schemas.TeamOfActorsShow | None = None
+    location_id: UUID | None = None
     try:
         token_data = authentication.verify_access_token(AuthorizationTypes.department, token)
+        location_id = db_services.Department.get(token_data.id).location.id
     except Exception as e:
         try:
             token_data = authentication.verify_access_token(AuthorizationTypes.actor, token)
             team_of_actors_id = websocket.headers.get("team_of_actors_id")
             team_of_actors = db_services.Actor.get_team_of_actors(team_of_actors_id)
+            location_id = team_of_actors.location.id
         except Exception as e:
             await websocket.close(code=status.WS_1008_POLICY_VIOLATION)
             return
 
-    await MessageHandler.user_joined_message(token_data, websocket, team_of_actors)
+    await MessageHandler.user_joined_message(token_data, websocket, team_of_actors, location_id)
     try:
         while True:
             data = await websocket.receive_text()
             print(f'{token=}')
             print(f'{json.loads(data)=}')
             message = json.loads(data)['chat-message']
-            await MessageHandler.handle_message(message, websocket, token_data, team_of_actors)
+            await MessageHandler.handle_message(message, websocket, token_data, team_of_actors, location_id)
     except WebSocketDisconnect as e:
-        await MessageHandler.user_leave_message(token_data, websocket, team_of_actors)
+        await MessageHandler.user_leave_message(token_data, websocket, team_of_actors, location_id)
