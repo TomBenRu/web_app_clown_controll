1a3ab5a64773f37907b6e3df243455e963137eb0
tombe <mail@thomas-ruff.de>
2024-02-16 19:45:47 +0100

    In progress.

--- routers/connection_manager.py
+++ routers/connection_manager.py
@@ -114,8 +114,10 @@ class MessageHandler:
             await manager.connect(websocket, False, location_id)
             actors = ', '.join([a.artist_name for a in team_of_actors.actors])
             message_to_departments = templates.get_template('responses/alert_clowns_team_joined.html.j2').render(team=f'Clowns-Team: {actors}')
+            note_presence = templates.get_template('responses/note_clowns_teams_presence.html.j2').render(text_teams=actors)
 
             await manager.send_alert_to_departments(websocket, message_to_departments, location_id)
+            await manager.broadcast_departments(note_presence, websocket, location_id)
             await manager.send_personal_clowns_team_message_departments_joined(websocket, location_id)
 
     @staticmethod
