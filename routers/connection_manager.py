208debb56ad4fd46b83aa073b2701e5540e0533f
tombe <mail@thomas-ruff.de>
2024-02-18 17:57:22 +0100

    In progress.

--- routers/connection_manager.py
+++ routers/connection_manager.py
@@ -32,6 +32,7 @@ class ConnectionManager:
                 del self.disconnected_clowns_teams[location_id][t_of_a_id]
                 if not self.disconnected_clowns_teams[location_id]:
                     del self.disconnected_clowns_teams[location_id]
+                print(f'!!!!!!!!!!!!!!!!!!!!!!!!!!! {self.disconnected_clowns_teams=}')
 
     def disconnect(self, websocket: WebSocket, department: bool, location_id: UUID, connection_lost: bool):
         if department:
@@ -41,6 +42,7 @@ class ConnectionManager:
             if connection_lost:
                 # self.disconnected_clowns_teams[location_id].append(websocket.headers.get("team_of_actors_id"))
                 self.disconnected_clowns_teams[location_id][websocket.headers.get("team_of_actors_id")] = []
+                print(f'!!!!!!!!!!!!!!!!!!!!!!!!!!! {self.disconnected_clowns_teams=}')
             else:
                 cmd_actor.DeleteTeamOfActors(UUID(websocket.headers.get("team_of_actors_id"))).execute()
 
